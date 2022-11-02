94fa4a351117f35918513cb890d328d9a2fbb8b2
andreanynthn <88206783+andreanynthn@users.noreply.github.com>
2022-11-02 12:29:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import uuid
 import numpy as np
 from PIL import Image
 import streamlit as st
@@ -8,10 +9,10 @@ import streamlit as st
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # create 'dataset' directory
-if 'dataset' in os.listdir(os.getcwd()):
-    pass
-else:
-    os.mkdir(os.path.join(os.getcwd(), 'dataset'))
+# if 'dataset' in os.listdir(os.getcwd()):
+#     pass
+# else:
+#     os.mkdir(os.path.join(os.getcwd(), 'dataset'))
 
 
 def loadDatabase(filename):
@@ -24,7 +25,7 @@ def loadDatabase(filename):
 
     data = database.read().split('\n')
     # names = [name for name in [data[i].split("'")[1] for i in range(len(data)-1)]]
-    names = [name for name in [data[i].split("'")[1] for i in range(len(data)-1)]]
+    names = [name for name in [data[i].split("'")[3] for i in range(len(data)-1)]]
     names.insert(0, 'None')
 
     return names
@@ -33,6 +34,7 @@ def loadDatabase(filename):
 def faceTraining(path):
 
     recognizer = cv2.face.LBPHFaceRecognizer_create()
+    recognizer.read('trainer.yml')
     detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
 
     imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
@@ -59,90 +61,87 @@ def faceTraining(path):
 def main():
     st.title("Face Recognition")
 
-    menu = ['Face Registration', 'Face Recognition']
-    choice = st.sidebar.selectbox("Menu", menu)
+    # menu = ['Face Registration', 'Face Recognition']
+    # choice = st.sidebar.selectbox("Menu", menu)
 
 
     # face registration
-    if choice == "Face Registration":
-        st.subheader("Face Registration")
+    # if choice == "Face Registration":
+    #     st.subheader("Face Registration")
+    #
+    #     image_file = st.file_uploader(
+    #         "Upload image", type = ["jpg", "png", "jpeg"]
+    #     )
+    #
+    #     if image_file is not None:
+    #
+    #         username = st.text_input("Input name")
+    #         faceId = st.text_input("Input id")
+    #         image = Image.open(image_file)
+    #
+    #         if st.button("Process"):
+    #
+    #             # process image
+    #
+    #             img = np.array(image.convert('RGB'))
+    #             img = cv2.cvtColor(img, 1)
+    #             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #             faces = face_detector.detectMultiScale(gray, 1.3, 5)
+    #
+    #             ids = open('faceId.txt', 'r')
+    #             listId = ids.read().split('\n')
+    #
+    #             for (x,y,w,h) in faces:
+    #                 cv2.rectangle(img, (x,y), (x+w+2,y+h+2), (255,0,0), 2)
+    #
+    #             if faceId in listId:
+    #                 st.error("Id already registered.")
+    #             else:
+    #                 with open('database.txt', 'a') as f:
+    #                     f.write(f'{faceId, username}\n')
+    #
+    #                 with open('faceId.txt', 'a') as f:
+    #                     f.write(f'{faceId}\n')
+    #                 cv2.imwrite(f"dataset/{username}." + faceId + ".jpg", gray[y:y+h,x:x+w])
+    #                 st.success("Successfully registered face.")
 
-        image_file = st.file_uploader(
-            "Upload image", type = ["jpg", "png", "jpeg"]
-        )
 
-        if image_file is not None:
+    image_file = st.file_uploader(
+        "Upload image", type = ["jpg", "png", "jpeg"]
+    )
 
-            username = st.text_input("Input name")
-            faceId = st.text_input("Input id")
-            image = Image.open(image_file)
+    if image_file is not None:
+        image = Image.open(image_file)
 
-            if st.button("Process"):
+        if st.button("Process"):
 
-                # process image
+            # process image
 
-                img = np.array(image.convert('RGB'))
-                img = cv2.cvtColor(img, 1)
-                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                faces = face_detector.detectMultiScale(gray, 1.3, 5)
+            img = np.array(image.convert('RGB'))
+            img = cv2.cvtColor(img, 1)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = face_detector.detectMultiScale(gray,
+                                                   scaleFactor = 1.2,
+                                                   minNeighbors = 5,
+                                                   minSize = (64, 48)
+                                                   )
+            path = 'dataset'
+            # executionPath = os.getcwd()
+            recognizer = faceTraining(path)
+            detector = "haarcascade_frontalface_default.xml"
+            faces = cv2.CascadeClassifier(detector)
+            names = loadDatabase('database.txt')
 
-                ids = open('faceId.txt', 'r')
-                listId = ids.read().split('\n')
+            for(x,y,w,h) in faces:
+                face_id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
 
-                for (x,y,w,h) in faces:
-                    cv2.rectangle(img, (x,y), (x+w+2,y+h+2), (255,0,0), 2)
-
-                if faceId in listId:
-                    st.error("Id already registered.")
+                if (round(100 - (confidence)) >= 50):
+                    name = names[face_id]
+                    confidence_score = "  {0}%".format(round(100 - confidence))
+                    st.success("Face found!")
+                    st.image(img, caption = f"Name: {name}; Confidence score: {confidence_score}")
                 else:
-                    with open('database.txt', 'a') as f:
-                        f.write(f'{faceId, username}\n')
-
-                    with open('faceId.txt', 'a') as f:
-                        f.write(f'{faceId}\n')
-                    cv2.imwrite(f"dataset/{username}." + faceId + ".jpg", gray[y:y+h,x:x+w])
-                    st.success("Successfully registered face.")
-
-
-    if choice == "Face Recognition":
-        st.subheader("Face Recognition")
-
-        image_file = st.file_uploader(
-            "Upload image", type = ["jpg", "png", "jpeg"]
-        )
-
-        if image_file is not None:
-            image = Image.open(image_file)
-
-            if st.button("Process"):
-
-                # process image
-
-                img = np.array(image.convert('RGB'))
-                img = cv2.cvtColor(img, 1)
-                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                faces = face_detector.detectMultiScale(gray,
-                                                       scaleFactor = 1.2,
-                                                       minNeighbors = 5,
-                                                       minSize = (64, 48)
-                                                       )
-                path = 'dataset'
-                # executionPath = os.getcwd()
-                recognizer = faceTraining(path)
-                detector = "haarcascade_frontalface_default.xml"
-                faceCascade = cv2.CascadeClassifier(detector)
-                names = loadDatabase('database.txt')
-
-                for(x,y,w,h) in faces:
-                    face_id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
-
-                    if (round(100 - (confidence)) >= 50):
-                        name = names[face_id]
-                        confidence_score = "  {0}%".format(round(100 - confidence))
-                        st.success("Face found!")
-                        st.image(img, caption = f"Name: {name}; Confidence score: {confidence_score}")
-                    else:
-                        st.error("Face not found!")
+                    st.error("Face not found!")
 
 if __name__ == '__main__':
     main()
